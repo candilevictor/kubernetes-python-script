2fdaf500145555699b256fe3c75817ccd7522d48
candilevictor <139927526+candilevictor@users.noreply.github.com>
2024-04-29 14:45:40 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+import time
+
+def main():
+  print("Iniciando script Python em cluster Kubernetes!")
+  time.sleep(10)
+  print("Script Python finalizado com sucesso!")
+
+if __name__ == "__main__":
+  main()
